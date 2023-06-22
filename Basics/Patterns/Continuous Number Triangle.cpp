0b70ffced6076b2a4fde08ca09b9d72131b39ec5
Manan Garg <manangarg1929@gmail.com>
2023-06-22 14:32:43 +0530

    Add files via upload

---
+++ Basics/Patterns/Continuous Number Triangle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i,j,n;
+    cin>>n;
+    int x=1;
+    for(i=1; i<=n; i++){
+        for(j=n; j>=i; j--){
+            cout<<" ";
+        }
+        for(j=1; j<=i; j++){
+                cout<<x<<" "; 
+                x++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
